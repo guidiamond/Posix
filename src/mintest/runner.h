894c2bcef9ad8e61c4d77519ba96e219b1e812a0
guidiamond <guilherme.batista770@gmail.com>
2020-06-21 15:20:09 -0300

    specify tests working

--- src/mintest/runner.h
+++ src/mintest/runner.h
@@ -1,30 +1,39 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <time.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char *argv[]) {
   clock_t start, end;
   double cpu_time_used;
   int size = sizeof(all_tests)/sizeof(test_data);
-  if (argc >= 2 && argc <= size) {
-    size = argc - 1;
+  if (argc > size || argv[1] == NULL) {
+    argc = 1;
+    argv[1] = "";
   }
 
   printf("Running %d tests:\n", size);
   printf("=====================\n\n");
   int pass_count = 0;
   for (int i = 0; i < size; i++) {
-    start = clock();
-    if (all_tests[i].function() >= 0) {
-      printf("%s: [PASS]", all_tests[i].name);
-      pass_count++;
-    };
-    end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("(%fs)\n", cpu_time_used);
+    if ((strcmp(all_tests[i].name,argv[1]) == 0 ) || (argc == 1)) {
+      start = clock();
+      if (all_tests[i].function() >= 0) {
+        printf("%s: [PASS]", all_tests[i].name);
+        pass_count++;
+      };
+      end = clock();
+      cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+      printf(" (%fs)\n", cpu_time_used);
+    }
   }
 
   printf("\n\n=====================\n");
-  printf("%d/%d tests passed\n", pass_count, size);
+  if (argc == 1) {
+    printf("%d/%d tests passed\n", pass_count, size);
+  }
+  else {
+    printf("%d/%d tests passed\n", pass_count, argc-1);
+  }
   return 0;
 }
